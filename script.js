21e7695cc50a9dfc9660d51699fbfefd1bcfe4a5
Your Name <JamieLou@jamie-revills-macbook.local>
2022-06-21 18:57:38 -0400

    adding arrays to character prompts

--- script.js
+++ script.js
@@ -11,30 +11,49 @@ function generatePassword() {
     alert("Password must be between 8 and 128 characters");
     generatePassword();
   }
+
+  else {var lcCharacters = prompt('Would you like to use lowercase letters? Enter "YES" or "NO" to choose.')
+
+  if (lcCharacters) {}
+
+  else {var ucCharacters = prompt("Would you like to use uppcercase letters?")}
+
+  if (ucCharacters) {}
+
+  else {var numCharacters = prompt("Would you like to use numeric characters?")}
+
+  if (numCharacters) {}
+
+  else {var sCharacters = prompt("Would you like to use special characters?")}
+};
+
   return "password";
 }
 
 //Asking if they want lower case, upper case, numeric, and/or special characters in the password
 
-function specialCharacters () {
-
 //Lower case letters
+function lowercaseCharacters () {
   const lcCharacters = Array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
-  var lcCharacters = prompt("Would you like to use lowercase letters?");
-  console.log(lcCharacters);
+}
 
 //Upper case letters
-  var ucCharacters = prompt("Would you like to use uppcercase letters?");
-  console.log(ucCharacters);
+function uppercaseCharacters () {
+  const lcCharacters = Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+}
+
 
 //Numeric characters
-  var numCharacters = prompt("Would you like to use numeric characters?");
-  console.log(numCharacters);
+function numericCharacters () {
+  const lcCharacters = Array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+}
+
 
 //Special characters
-  var sCharacters = prompt("Would you like to use special characters?");
-  console.log(sCharacters);
+function specialCharacters () {
+  const lcCharacters = Array('!', '@', '#', '$', '%', '^', '&', '*', '+', '=', '?');
 }
+
 
 // Write password to the #password input
 function writePassword() {
